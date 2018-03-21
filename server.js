86fdd3861eec0bc1f2dbc8680bd6ab38687840e4
Glitch (fcc-header-ms) <none>
2018-03-21 17:18:05 +0000

    🌛📮 Checkpoint ./server.js:704354/379

--- server.js
+++ server.js
@@ -16,22 +16,10 @@ app.get("/", (request, response) => {
   response.sendFile(__dirname + '/views/index.html')
 })
 
-// Simple in-memory store
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-]
-
-app.get("/dreams", (request, response) => {
-  response.send(dreams)
+app.get("/api/whoami", (request, response) => {
+  
 })
 
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", (request, response) => {
-  dreams.push(request.query.dream)
-  response.sendStatus(200)
-})
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
