84ddcd799b1fbbbc813f15cffd78452f2fa537f0
Glitch (fcc-header-ms) <none>
2018-03-22 05:48:52 +0000

    💘🎽 Checkpoint ./controllers/httpController.js:704354/221

--- controllers/httpController.js
+++ controllers/httpController.js
@@ -1,7 +1,6 @@
 exports.parse = function(req, res){
   var response = {};
   response.language = req.headers["accept-language"].split(",")[0];
-  var agent = req.headers["user-agent"];
-  response.software = agent.substring(agent.lastIndexOf("(") + 1, agent.lastIndexOf(")"));
+  response.ipaddress = req.headers["x-forwarded-for"].split(",")[0];
   res.json(response);
 }
