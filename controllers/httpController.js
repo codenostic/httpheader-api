3da27b82d3df1172349d06fa68b08d198a911fc4
Glitch (fcc-header-ms) <none>
2018-03-22 06:07:45 +0000

    new file

--- controllers/httpController.js
+++ controllers/httpController.js
@@ -3,6 +3,6 @@ exports.parse = function(req, res){
   response.ipaddress = req.headers["x-forwarded-for"].split(",")[0];
   response.language = req.headers["accept-language"].split(",")[0];
   var agent = req.headers['user-agent'];
-  response.software = agent.substring(agent.indexOf("(")+1, agent.indexOf(")"));
+  response.software = agent.substring(agent.indexOf("(")+1, agent.indexOf(") "));
   res.json(response);
 }
